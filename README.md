7860a4ce6da49d621fa1b5197264ba8bc4bc18d9
Joon Ho Cho <joonho1101@gmail.com>
2016-06-27 16:15:33 -0700

    Add node_modules

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # graphql-complexity
+[![Build Status](https://travis-ci.org/joonhocho/graphql-complexity.svg?branch=master)](https://travis-ci.org/joonhocho/graphql-complexity)
+[![Coverage Status](https://coveralls.io/repos/github/joonhocho/graphql-complexity/badge.svg?branch=master)](https://coveralls.io/github/joonhocho/graphql-complexity?branch=master)
+[![npm version](https://badge.fury.io/js/graphql-complexity.svg)](https://badge.fury.io/js/graphql-complexity)
+[![Dependency Status](https://david-dm.org/joonhocho/graphql-complexity.svg)](https://david-dm.org/joonhocho/graphql-complexity)
+[![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org)
+
 Compute complexity of GraphQL Queries
---
+++ test/index.js
@@ -0,0 +1,72 @@
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLObjectType,
+} from 'graphql';
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import complexity from '../lib';
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+      expense: 1,
+    },
+    name: {
+      type: GraphQLString,
+      expense: 2,
+    },
+    friends: {
+      type: UserType,
+      expense: 10,
+      args: {
+        limit: {
+          type: GraphQLInt,
+        },
+      },
+    },
+  }),
+});
+
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      user: {
+        type: UserType,
+        args: {
+          id: {
+            type: GraphQLInt,
+          },
+        },
+        resolve: (_, {id}, context, info) => {
+          console.log(JSON.stringify(info.operation.selectionSet, null, '  '));
+          console.log(
+            JSON.stringify(info.fieldASTs) ===
+            JSON.stringify(info.operation.selectionSet.selections)
+          );
+          // info.fieldASTs
+          // === info.operation.selectionSet.selections
+          return id;
+        },
+      },
+    },
+  }),
+});
+
+
+const runQuery = (query) =>
+  graphql(schema, query)
+    .then((res) => res.data.input);
+
+
+describe('GraphQLProtection', () => {
+  it('default', (done) => {
+    runQuery(`{ user(id: 7) { id name friends(limit: 10) { id name friends { id name } } } }`).then(done, done);
+  });
+});
